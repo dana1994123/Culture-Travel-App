322d6f448efcb177f4f6fd093ffdc48646cddecb
dana <danaaljamal94@gmail.com>
2020-11-05 00:27:59 -0500

    added the app arch

--- app/src/main/java/com/example/myapplication/views/HomeActivity2.kt
+++ app/src/main/java/com/example/myapplication/views/HomeActivity2.kt
@@ -1,9 +1,7 @@
-package com.example.myapplication
+package com.example.myapplication.views
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,6 +11,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.example.myapplication.R
 
 class HomeActivity2 : AppCompatActivity() {
 
---
+++ app/src/main/java/com/example/myapplication/views/SignInActivity.kt
@@ -0,0 +1,134 @@
+package com.example.myapplication.views
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+import com.example.myapplication.managers.SharedPreferencesManager
+import com.example.myapplication.utils.DataValidations
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+
+
+class SignInActivity : AppCompatActivity() ,View.OnClickListener {
+
+    val TAG : String = this@SignInActivity.toString()
+    lateinit var tvCreateAccount: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+
+        tvCreateAccount = findViewById(R.id.createAccount)
+        tvCreateAccount.setOnClickListener(this)
+
+        loginBtn.setOnClickListener(this)
+
+
+
+        SharedPreferencesManager.init(applicationContext)
+        this.fetchPreferences()
+    }
+
+    override fun onClick(v: View?) {
+
+        if (v!=null){
+            when (v.id){
+                loginBtn.id ->{
+                    if(this.validateData()){
+                        this.validateUser()
+                        this.goToHome()
+                    }
+                }
+                tvCreateAccount.id ->{
+                    this.goToSignUp()
+                }
+            }
+        }
+    }
+
+
+    private fun validateUser(){
+        // we have to validate the information that the user has been input by
+        // searching our database
+    }
+
+
+
+    private fun validateData() : Boolean {
+
+
+        if (edtEmail.text.isEmpty()){
+            edtEmail.setError("Email cannot be empty")
+            return false
+        }
+
+        if (edtPassword.text.isEmpty()){
+            edtPassword.setError("Password cannot be empty")
+            return false
+        }
+
+        if (!DataValidations().validateEmail(edtEmail.text.toString())){
+            edtEmail.setError("Please provide valid email address")
+            return false
+        }
+
+        return true
+    }
+
+
+
+
+
+
+
+    fun goToHome(){
+        val intentGoToHome = Intent(this, HomeActivity2::class.java)
+        startActivity(intentGoToHome)
+
+    }
+
+
+
+    private fun checkRemember(){
+        if (swtRemember.isChecked){
+            //save the credentials in shared preferences
+            SharedPreferencesManager.write(SharedPreferencesManager.EMAIL, edtUsername.text.toString())
+            SharedPreferencesManager.write(SharedPreferencesManager.PASSWORD, edtPassword.text.toString())
+        }else{
+            //remove the credentials from shared preferences
+            SharedPreferencesManager.remove(SharedPreferencesManager.EMAIL)
+            SharedPreferencesManager.remove(SharedPreferencesManager.PASSWORD)
+        }
+    }
+
+
+
+
+    private fun fetchPreferences(){
+        edtUsername.setText(SharedPreferencesManager.read(SharedPreferencesManager.EMAIL, ""))
+        edtPassword.setText(SharedPreferencesManager.read(SharedPreferencesManager.PASSWORD,""))
+    }
+
+
+
+    private fun goToSignUp(){
+        val signUpIntent = Intent(this, SignUpActivity::class.java)
+        startActivity(signUpIntent)
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
