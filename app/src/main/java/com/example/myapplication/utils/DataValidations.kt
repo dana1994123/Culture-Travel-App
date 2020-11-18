710fd65d456271ef0a1eca2aaacbc4517a98b174
Ofir David <davidof@sheridancollege.ca>
2020-11-18 11:39:54 -0500

    added data validations to the PaymentFragment.kt

--- app/src/main/java/com/example/myapplication/utils/DataValidations.kt
+++ app/src/main/java/com/example/myapplication/utils/DataValidations.kt
@@ -15,7 +15,16 @@ class DataValidations {
         return !TextUtils.isEmpty(email) &&
                 Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
-
+    fun validatePhonenNum(phone:String):Boolean{
+        return !TextUtils.isEmpty(phone) && Patterns.PHONE.matcher(phone).matches()
+    }
+    fun validateName(name:String):Boolean{
+        return !TextUtils.isEmpty(name) && name.chars().allMatch(Character::isLetter)
+    }
+    fun validateExpiryDate(expiryDate:String):Boolean{
+        return !TextUtils.isEmpty(expiryDate) ||
+                expiryDate.matches("(?:0[1-9]|1[0-2])/[0-9]{2}".toRegex())
+    }
     fun encryptPassword(password: String) : String{
         try{
             val md = MessageDigest.getInstance("SHA-256")
@@ -31,4 +40,5 @@ class DataValidations {
 
         return ""
     }
+
 }
