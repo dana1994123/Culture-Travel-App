d5755f0002ecaa4698fab5a7e9171e483036a50f
roopkaurmalhans <malhanro@sheridancollege.ca>
2020-11-30 11:27:38 -0500

    Rating Bar added

--- app/src/main/java/com/example/myapplication/ui/support/AboutFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/support/AboutFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.support
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -21,7 +22,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [AboutFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AboutFragment : Fragment(),View.OnClickListener{
+class AboutFragment : Fragment(){
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -31,11 +32,12 @@ class AboutFragment : Fragment(),View.OnClickListener{
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
-
+     //       onClickRating()
 
         }
 
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,17 +45,10 @@ class AboutFragment : Fragment(),View.OnClickListener{
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
-//        val root = inflater.inflate(R.layout.fragment_about, container, false)
-//        rating.setOnRatingBarChangeListener{
-//                ratingBar, fl, b ->
-//            tv_4.text= "You rated us " + fl.toString() + " Thank you for feedback!!"
-//        }
-//        val rate= root.findViewById<View>(R.id.rating)as RatingBar
-//      //  val submit = root.findViewById<View>(R.id.tv_3)as Button
-//        tv_4.text= "Thank you for feedback"
 
-     //   return root
     }
+
+
 
     companion object {
         /**
@@ -74,10 +69,4 @@ class AboutFragment : Fragment(),View.OnClickListener{
                 }
             }
     }
-
-    override fun onClick(p0: View?) {
-
-    }
-
-
 }
