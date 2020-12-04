040b8364b50af440bfcf4feed1631726acc1cb65
roopkaurmalhans <malhanro@sheridancollege.ca>
2020-12-04 10:43:09 -0500

    added permission for camera

--- app/src/main/java/com/example/myapplication/ui/support/AboutFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/support/AboutFragment.kt
@@ -8,8 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RatingBar
+import android.widget.TextView
 import android.widget.Toast
 import com.example.myapplication.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_about.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -26,17 +28,13 @@ class AboutFragment : Fragment(){
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    lateinit var tvRating: TextView
+    lateinit var rating : RatingBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
-            //rating is id from xml file
-//            rating.setOnRatingBarChangeListener { ratingBar, fl, b ->
-//                tv_4.setText("$fl")
-//            }
-
         }
 
     }
@@ -47,8 +45,13 @@ class AboutFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
-
+        val root = inflater.inflate(R.layout.fragment_about, container, false)
+        rating = root.findViewById(R.id.rating)
+        rating.setOnRatingBarChangeListener { ratingBar, fl, b ->
+            tvRating = root.findViewById(R.id.tv_4)
+            tvRating.text = "Your rating $fl.Thank you for providing your feedback!!"
+          }
+        return root
     }
 
 
