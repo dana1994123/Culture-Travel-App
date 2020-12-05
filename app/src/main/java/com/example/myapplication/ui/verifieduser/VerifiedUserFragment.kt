251dbe4f99972731bea07caff7cfd971f2129d43
dana <danaaljamal94@gmail.com>
2020-12-05 12:56:21 -0500

    Verified UserUi

--- app/src/main/java/com/example/myapplication/ui/verifieduser/VerifiedUserFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/verifieduser/VerifiedUserFragment.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.R
 
-class VerifiedUserFragment : Fragment() {
+class VerifiedUserFragment : Fragment() , View.OnClickListener{
+    private lateinit var verifiedBtn : Button
 
     private lateinit var verifiedUserViewModel: VerifiedUserViewModel
 
@@ -22,10 +24,17 @@ class VerifiedUserFragment : Fragment() {
         verifiedUserViewModel =
                 ViewModelProvider(this).get(VerifiedUserViewModel::class.java)
         val root = inflater.inflate(R.layout.verified_user, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        verifiedUserViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        verifiedBtn = root.findViewById(R.id.verifiedBtn)
         return root
+    }
+
+    override fun onClick(v: View?) {
+        if(v !=null){
+            when (v.id){
+                verifiedBtn.id ->{
+                    //take a picture change the verification status for each user and then show the verification sign
+                }
+            }
+        }
     }
 }
