0c4039999b94915a7bac4e16c5ce54313dd6aec0
dana <danaaljamal94@gmail.com>
2020-12-05 13:40:06 -0500

    Verified User taking an id picture

--- app/src/main/java/com/example/myapplication/models/Guest.kt
+++ app/src/main/java/com/example/myapplication/models/Guest.kt
@@ -3,12 +3,11 @@ package com.example.myapplication.models
 import java.util.*
 
 data class Guest (
-        //add the id
         var id: String = UUID.randomUUID().toString(),
         var name :String ="",
         var email:String = "",
         var password :String = "",
         var phoneNumber : String? ="",
-        var language : String? = ""
-
+        var language : String? = "",
+        var verifiedStatus :Boolean = false
 )
--- app/src/main/java/com/example/myapplication/ui/verifieduser/VerifiedUserFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/verifieduser/VerifiedUserFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.verifieduser
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -25,14 +26,30 @@ class VerifiedUserFragment : Fragment() , View.OnClickListener{
                 ViewModelProvider(this).get(VerifiedUserViewModel::class.java)
         val root = inflater.inflate(R.layout.verified_user, container, false)
         verifiedBtn = root.findViewById(R.id.verifiedBtn)
+        verifiedBtn.setOnClickListener(this)
         return root
     }
 
     override fun onClick(v: View?) {
         if(v !=null){
             when (v.id){
-                verifiedBtn.id ->{
+                verifiedBtn.id -> {
                     //take a picture change the verification status for each user and then show the verification sign
+                    val actionItems = arrayOf("Scan your Id", "Choose from Gallery", "Cancel")
+                    var alertDialog = AlertDialog.Builder(this.requireContext())
+                    alertDialog.setTitle("Become Verified User")
+                    alertDialog.setItems(actionItems) { dialog, index ->
+                        if (actionItems.get(index).equals("Scan your Id")) {
+
+
+                        } else if (actionItems.get(index).equals("Choose from Gallery")) {
+
+                        } else if (actionItems.get(index).equals("Cancel")) {
+                            dialog.dismiss()
+                        }
+                    }
+                    alertDialog.show()
+
                 }
             }
         }
