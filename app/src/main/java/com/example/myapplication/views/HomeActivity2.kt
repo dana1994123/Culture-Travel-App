ee4605eafe289b831318687f6084debd38f09c8a
dana <danaaljamal94@gmail.com>
2020-12-06 13:36:58 -0500

    user information

--- app/src/main/java/com/example/myapplication/views/HomeActivity2.kt
+++ app/src/main/java/com/example/myapplication/views/HomeActivity2.kt
@@ -31,6 +31,8 @@ import androidx.navigation.NavController
 import com.example.myapplication.R
 //import com.example.myapplication.LocationManager
 import com.example.myapplication.managers.SharedPreferencesManager
+import com.example.myapplication.models.Guest
+import com.example.myapplication.viewmodels.ViewModels
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -47,7 +49,8 @@ class HomeActivity2 : AppCompatActivity(),View.OnClickListener{
     private lateinit var email: TextView
     private lateinit var imgProfilePic: ImageView
     private val REQUEST_GALLERY_PICTURE=172
-    private val currentUser = SharedPreferencesManager.read(SharedPreferencesManager.EMAIL,"").toString()
+    private lateinit var viewModel : ViewModels
+    private lateinit var  currentUser : Guest
 
 
 
@@ -56,6 +59,9 @@ class HomeActivity2 : AppCompatActivity(),View.OnClickListener{
         setContentView(R.layout.activity_home2)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        viewModel = ViewModels()
+        viewModel.fetchAllGuest()
 
         drawerLayout= findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
@@ -73,6 +79,8 @@ class HomeActivity2 : AppCompatActivity(),View.OnClickListener{
         name= headerLayout.findViewById(R.id.edtUserName)
         imgProfilePic=headerLayout.findViewById(R.id.imgProfilePic)
         imgProfilePic.setOnClickListener(this)
+
+        this.getUserInfo()
 
 
     }
@@ -145,4 +153,16 @@ class HomeActivity2 : AppCompatActivity(),View.OnClickListener{
             }
         }
     }
+
+
+    fun getUserInfo(){
+        viewModel.guestList.observe(this,{
+            if(it != null){
+                currentUser = it[0]
+                edtUserName.setText(currentUser.name)
+                edtEmail.setText(currentUser.email)
+            }
+        })
+    }
+
 }
